c838aa4dcc623768af97f2b2e74d811afdf189e5
sorna3126 <sornak3105@gmail.com>
2023-11-15 22:02:04 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def func(request):
+    return render(request,'ex1.html')
+def virat(request):
+    return render(request,'ex2.html')
+def msd(request):
+    return render(request,'msd.html')
+def rohit(request):
+    return render(request,'rohit.html')
